fce9ff1f52b87da92511b0ebada5a3681c2ff37a
Your <kawin_chusin@cmu.ac.th>
2023-01-03 16:19:09 +0700

    lab11

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int count = 0;
+    float sum =0;
+    float sum2 =0;
+    string textline;
+    ifstream source("score.txt");
+    while(getline(source,textline)){
+        sum += atof(textline.c_str());
+        sum2 += pow(atof(textline.c_str()),2);
+        count++;
+    }
+    cout <<"Number of data = "<< count<<'\n';
+    cout << setprecision(3);
+    cout << "Mean = "<< sum/count<<'\n';
+    cout << "Standard deviation = "<<sqrt((sum2/count)-pow(sum/count,2));
+    source.close();
+    return 0;
+
+}
